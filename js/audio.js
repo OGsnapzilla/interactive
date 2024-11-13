f52d153318ce4f5b064c233c9e1640b6f1b21b0f
KamillaR <124995176+OGsnapzilla@users.noreply.github.com>
2024-11-13 12:02:38 +0100

    fix fejl

--- js/audio.js
+++ js/audio.js
@@ -1,1 +1,34 @@
-au
+"use strict";
+
+//henter audio elementet og knappen
+const audio = document.getElementById("audio");
+const audioBtn = document.getElementById("audio-btn");
+
+//Laver tænd/sluk funktion
+audioBtn.addEventListener("click", () =>{
+    if (audio.muted) {
+        audio.muted = false; 
+        audioBtn.textContent = "Sluk musik";
+    }
+    else{
+        audio.muted = true;
+        audioBtn.textContent = "Tænd musik";
+    }
+});
+
+//henter knappene og popup
+const popup = document.getElementById('pop-up-question');
+const openPopupBtn = document.getElementById('open-btn');
+const closePopupBtn = document.getElementById('close-btn-popup');
+
+//klikk gjør at popup blir synlig eller gjemt
+openPopupBtn.addEventListener('click', () => {
+    popup.classList.remove('hidden');
+    popup.classList.add('visible');
+
+});
+
+closePopupBtn.addEventListener('click', () => {
+    popup.classList.remove('visible');
+    popup.classList.add('hidden');
+});
